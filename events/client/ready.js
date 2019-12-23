32a0a62c2221d4b78f1c22c4f596eba5ec2e202c
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 00:19:50 +0200

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,16 @@
+module.exports = async bot => {
+     console.log(`${bot.user.username} is online`)
+
+    let statuses = [
+        "porn",
+        "!help",
+        `over ${bot.users.size} users!`
+    ]
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, {type: "WATCHING"});
+
+    }, 5000)
+
+}
