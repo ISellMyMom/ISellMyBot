fae4f19aa2876b33ff069c7819eb4c85c0ee6ff9
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 21:31:36 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -11,8 +11,20 @@ module.exports = {
 
     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Nu poți folosi aceasta comanda!")
     
-    let messagecount = parseInt(numberofmessages);
-    message.channel.fetchMessages({ limit: messagecount }).then(messages => message.channel.bulkDelete(messages));
-
+    const user = message.mentions.users.first();
+// Parse Amount
+const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+if (!amount) return message.reply('Must specify an amount to delete!');
+if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+// Fetch 100 messages (will be filtered and lowered up to max amount requested)
+message.channel.fetchMessages({
+ limit: 100,
+}).then((messages) => {
+ if (user) {
+ const filterBy = user ? user.id : Client.user.id;
+ messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+ }
+ message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+});
     }
 }
