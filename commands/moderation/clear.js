d8ed861d52ba468c62ac2f076df93c1e538b6553
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 20:40:22 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,4 +1,19 @@
-message.channel.fetchMessages()
+module.exports = {
+    config: {
+        name: "clear",
+        description: "Șterge mesajele.",
+        usage: "linii",
+        category: "moderation",
+        accessableby: "Moderator",
+        aliases: ["sterge", "clean"]
+    },
+    run: async (bot, message, args) => {
+
+    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Nu poți folosi aceasta comanda!")
+    
+
+    message.channel.fetchMessages()
                .then(function(list){
                     message.channel.bulkDelete(list);
                 }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+}
