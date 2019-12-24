313fa14cff18c4fe8e9ff44845c631f2930a093e
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 20:37:40 +0200

    Create clear.js

---
+++ commands/moderation/clear.js
@@ -0,0 +1,4 @@
+message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
