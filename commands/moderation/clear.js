6c4b484b5275a92966371e82ecae08be21a1718e
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 21:49:58 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -11,14 +11,24 @@ module.exports = {
 
     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Nu poți folosi aceasta comanda!")
     
-    message.channel.fetchMessages({
-        limit: messagecount
-    }).then(messages => message.channel.bulkDelete(messages));
-
-    message.channel.send('Messages purged.')
-        .then(msg => {
-            msg.delete()
-        });
+    const deleteCount = parseInt(args[0], 10);
+    async function clear() {
+        // get the delete count, as an actual number.
+        if(!message.member.hasPermission("MANAGE_MESSAGES")){
+          message.channel.send("You don't have the permissions to use this command!");
+        }
+        
+        else{        
+          // Ooooh nice, combined conditions. <3
+          if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.channel.send("Please provide a number between 2 and 100 for the number of messages to delete");
+          
+          message.delete();
+          const fetched = await message.channel.fetchMessages({limit: deleteCount});
+          message.channel.bulkDelete(fetched).catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+        }
+    }
+    
 
 
     }
