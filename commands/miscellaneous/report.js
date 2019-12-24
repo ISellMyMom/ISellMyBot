e13eccf55c56469c7e356718606b5f56949cb5c0
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 18:35:48 +0200

    Update report.js

--- commands/miscellaneous/report.js
+++ commands/miscellaneous/report.js
@@ -1,28 +1,29 @@
+const { report_channel } = require("../../data/config.json");
 module.exports = { 
     config: {
         name: "report",
-        description: "reports a user of the guild",
-        usage: "!report <user> <reason>",
-        accessableby: "Members",
+        description: "Raporteaza un membru pentru ca e hacherman.",
+        usage: "utilizator motiv",
+        accessableby: "Membru",
     },
     run: async (bot, message, args) => {
 
         message.delete()
         // mentioned or grabbed user
         let target = message.mentions.members.first() || message.guild.members.get(args[0])
-        if(!target) return message.channel.send("Please provide a valid user").then(m => m.delete(15000))
+        if(!target) return message.channel.send("Utilizator invalid").then(m => m.delete(15000))
 
         // reasoning definition
         let reason = args.slice(1).join(" ")
-        if(!reason) return message.channel.send(`Please provide a reason for reporting **${target.user.tag}**`).then(m => m.delete(15000))
+        if(!reason) return message.channel.send(`Scrie si motivul pentru care il raportezi pe **${target.user.tag}**`).then(m => m.delete(15000))
 
         // grab reports channel
-        let sChannel = message.guild.channels.find(x => x.name === "tut-reports")
+        let sChannel = message.guild.channels.find(x => x.name === report_channel)
 
         // send to reports channel and add tick or cross
 
-        message.channel.send("Your report has been filed to the staff team. Thank you!").then(m => m.delete(15000))
-        sChannel.send(`**${message.author.tag}** has reported **${target.user.tag}** for **${reason}**.`).then(async msg => {
+        message.channel.send("Multumesc ca ai raportat acest utilizator.").then(m => m.delete(15000))
+        sChannel.send(`**${message.author.tag}** l-a raportat pe **${target.user.tag}** pentru **${reason}**.`).then(async msg => {
             await msg.react("✅")
             await msg.react("❌")
         })
