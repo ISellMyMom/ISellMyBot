ae5a716adc4b183e9158268e8c676afd2aab2345
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 12:06:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ const bot = new Client();
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN)
