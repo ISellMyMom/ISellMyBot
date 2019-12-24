2e5c7eda3872bf3f9aad52a54f88e83839037013
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 21:44:48 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -11,16 +11,15 @@ module.exports = {
 
     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Nu poți folosi aceasta comanda!")
     
-    const user = message.mentions.users.first();
-// Parse Amount
-const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-if (!amount) return message.reply('Must specify an amount to delete!');
-if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-// Fetch 100 messages (will be filtered and lowered up to max amount requested)
-message.channel.fetchMessages({
- limit: 100,
-}).then((messages) => {
- message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
+    message.channel.fetchMessages({
+        limit: messagecount
+    }).then(messages => message.channel.bulkDelete(messages));
+
+    message.channel.send('Messages purged.')
+        .then(msg => {
+            msg.delete()
+        });
+
+
     }
 }
