f6cf5ffffe4ca08f6fbcbd05dabd8b6288d6d499
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 18:30:10 +0200

    Update ping.js

--- commands/moderation/ping.js
+++ commands/moderation/ping.js
@@ -1,13 +1,15 @@
 module.exports = { 
     config: {
         name: "ping",
-        description: "Arata ping-ul botulu",
+        description: "Arata ping-ul botului.",
         usage: "",
         category: "moderation",
         accessableby: "Moderator"
     },
     run: async (bot, message, args) => {
-
+        
+    if(!message.member.hasPermission("MANAGE_ROLES") || !message.guild.owner) return message.channel.send("Nu ai permisiunea sa folosesti aceasta comanda.");
+        
     message.channel.send("Pinging...").then(m => {
         let ping = m.createdTimestamp - message.createdTimestamp
         let choices = ["Asta-i ping-ul meu", "Ii ok? Nu ma pot uita", "Sper ca nu-i asa rau", "Coaie, n-are cum numai atat"]
