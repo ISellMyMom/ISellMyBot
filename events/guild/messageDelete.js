b176b3b95fafd67dac65a7432ca30edc84715f99
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-24 13:27:08 +0200

    Update messageDelete.js

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,4 +1,4 @@
-
+const { log_channel } = require("./data/config.json");
 module.exports = async (bot, message) => {
-	bot.channels.get("646804988139536413").send("Message: ``" + message.content + "`` sent in ``#" + message.channel.name + "`` submited by ``" + message.author.tag + "`` was deleted.");
+	bot.channels.get(log_channel).send("Message: ``" + message.content + "`` sent in ``#" + message.channel.name + "`` submited by ``" + message.author.tag + "`` was deleted.");
 }
