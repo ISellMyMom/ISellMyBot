68e31ada36369d44f579f3dad3c657511e075ec5
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-25 04:25:15 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -14,8 +14,8 @@ module.exports = {
     const deleteCount = parseInt(args[0], 10);
     async function clear() {       
           // Ooooh nice, combined conditions. <3
-          if(!deleteCount || deleteCount < 1 || deleteCount > 100)
-            return message.channel.send("Alege un număr între 1 și 100 and 100.");
+          if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.channel.send("Alege un număr între 2 și 100 and 100.");
           
           message.delete();
           const fetched = await message.channel.fetchMessages({limit: deleteCount});
