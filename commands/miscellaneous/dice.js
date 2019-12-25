0cb371b31af8fa156898ce07d64dbce68f4bcf2d
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-26 01:29:31 +0200

    Create dice.js

---
+++ commands/miscellaneous/dice.js
@@ -0,0 +1,16 @@
+const { dice }= require("../../data/definedMessages.json");
+
+module.exports = {
+    config: {
+        name: "dice",
+        description: "Dai cu zaru' 6-5, n-ai nevoie de servici.",
+        usage: "",
+        category: "miscellaneous",
+        accessableby: "Membru",
+        aliases: ["barbut", "zar"]
+    },
+    run: async (bot, message, args) => {
+		var xdice = dice[Math.floor(Math.random() * dice.length)];
+		message.channel.send(xdice);
+    }
+}
