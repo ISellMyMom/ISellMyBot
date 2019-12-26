5ed8ff250f5900aabcbd96f83b4da47771d82748
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-26 11:54:54 +0200

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,6 +1,7 @@
 const { RichEmbed } = require("discord.js")
 const { redlight } = require("../../data/colours.json");
 const { channel } = require("../../data/config.json");
+const { error } = require("../../data/definedMessages.json");
 
 module.exports = {
     config: {
@@ -21,7 +22,7 @@ module.exports = {
    let reason = args.slice(1).join(" ");
    if(!reason) reason = "No reason given!"
 
-   if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command")
+   if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send(error.permission)
 
    banMember.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`).then(() =>
    message.guild.ban(banMember, { days: 1, reason: reason})).catch(err => console.log(err))
@@ -37,7 +38,7 @@ module.exports = {
     .addField("Reason:", reason)
     .addField("Date:", message.createdAt.toLocaleString())
     
-        let sChannel = message.guild.channels.find(c => c.name === channel.log)
+        let sChannel = message.guild.channels.find(c => c.name === channel.banlog)
         sChannel.send(embed)
     }
 }
