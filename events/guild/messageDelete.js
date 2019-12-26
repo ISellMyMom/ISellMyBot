2257459045847e7590d1fb17f8357b5cb6653a48
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-26 11:06:36 +0200

    Update messageDelete.js

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,4 +1,7 @@
 const { channel } = require("../../data/config.json");
 module.exports = async (bot, message) => {
+        if(message.content.includes("clear")) return;
+	if(message.channel.name == newbie_channel) return;
+
 	bot.channels.find('name', channel.log).send("Mesajul ``" + message.content + "`` din ``#" + message.channel.name + "`` trimis de ``" + message.author.tag + "`` a fost sters.");
 }
