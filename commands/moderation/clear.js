b810f185f0119d2c9c3ddc38b2ce876668d1a11c
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-26 12:18:20 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -17,7 +17,7 @@ module.exports = {
     async function clear() {       
           // Ooooh nice, combined conditions. <3
           if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.channel.send("Alege un număr între 2 și 100 and 100.");
+            return message.channel.send("Alege un număr între 2 și 100.");
           
           message.delete();
           const fetched = await message.channel.fetchMessages({limit: deleteCount});
@@ -25,7 +25,7 @@ module.exports = {
     }
     clear();
     let sChannel = message.guild.channels.find(c => c.name === channel.messageslog)
-    sChannel.send("Au fost șterse ``" + deleteCount + "`` mesaje din canalul ``" + message.channel.name` + "``.")
+    //sChannel.send("Au fost șterse ``" + deleteCount + "`` mesaje din canalul ``" + message.channel.name` + "``.")
 
 
     }
