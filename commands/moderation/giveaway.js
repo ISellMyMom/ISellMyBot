f9c884b5e32253152c183b4c9b349f6e64627312
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2019-12-31 10:05:06 +0200

    Update giveaway.js

--- commands/moderation/giveaway.js
+++ commands/moderation/giveaway.js
@@ -1,6 +1,3 @@
-const g_channel_id = '659688758542860308'; // This should be a String and the channel ID of the giveaways channel
-const g_message_id = ''; // This should be a String and the message ID of the givaway message
-const winner_amount = ''; // This shiuld be a Number and it represents how many winners you wish to choose
 
 module.exports = { 
     config: {
@@ -24,7 +21,7 @@ module.exports = {
     let users = reaction.users.map(u => u.toString());
     return message.channel.send(`**${users.random()}** has won the giveaway!`); // this uses the Array.prototpe.random method below
   
-    const c = message.guild.channels.get(g_channel_id);
+    const ch = message.guild.channels.get(g_channel_id);
     const m = await c.fetchMessage(g_message_id);
     const reactions = m.reactions;
     const reaction = reactions.first(); // This reaction would normally be the reaction a user would use to enter the giveaway
@@ -38,7 +35,7 @@ module.exports = {
     	if (winner_amount === 2) winners = selected.join(' and ');
     	else winners = selected.join(', ');
 
-    	return c.send(`**${winners}** have won the giveaway!`);
+    	return ch.send(`**${winners}** have won the giveaway!`);
     }
 Array.prototype.random = function () {
   return this[Math.floor(Math.random() * this.length)];
