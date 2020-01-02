23e114d2635f0400fede1ae0f486058d3043c2f1
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2020-01-02 20:39:26 +0200

    Update messageDelete.js

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,7 +1,12 @@
 const { channel } = require("../../data/config.json");
+const { RichEmbed } = require("discord.js");
+const { cream } = require("../../data/colours.json");
 module.exports = async (bot, message) => {
-        if(message.content.includes("clear")) return;
+        if(message.content.includes("clear", "report")) return;
 	if(message.channel.name ==  channel.newbie) return;
+        let sEmbed = new RichEmbed()
+            .setColor(cream)
+            .setDescription("Mesajul ``" + message.content + "`` din ``#" + message.channel.name + "`` a fost șters de ``" + message.author.tag + "``");
 
-	bot.channels.find(x => x.name === channel.messageslog).send("Mesajul ``" + message.content + "`` din ``#" + message.channel.name + "`` trimis de ``" + message.author.tag + "`` a fost sters.");
+	bot.channels.find(x => x.name === channel.messageslog).send(sEmbed);//("Mesajul ``" + message.content + "`` din ``#" + message.channel.name + "`` trimis de ``" + message.author.tag + "`` a fost sters.");
 }
