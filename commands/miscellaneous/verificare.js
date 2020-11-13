97230822700559dc896ea7b0cbc7c67c621fee0e
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2020-11-13 12:00:57 +0200

    Create verificare.js

---
+++ commands/miscellaneous/verificare.js
@@ -0,0 +1,19 @@
+const agree = "✅";
+const { channeles } = require("../../data/config.json");
+const { welcome } = require("../../data/definedMessages.json");
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    config: {
+        name: "verificare",
+        description: "Verifica daca esti un robot. Beep Boop?",
+        usage: "",
+        category: "miscellaneous",
+        accessableby: "Membru",
+        aliases: [""]
+    },
+    run: async (bot, message, args) => {
+        message.channel.send("Sistemul de verificare este in mentenanță");
+  
+    }
+}
