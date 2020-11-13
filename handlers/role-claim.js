68e7e4f3a897db9092c255a0e129ef6176ffae8d
ISellMyMom <59160961+ISellMyMom@users.noreply.github.com>
2020-11-13 03:28:33 +0200

    Update role-claim.js

--- handlers/role-claim.js
+++ handlers/role-claim.js
@@ -2,5 +2,60 @@ const firstMessage = require('./first-message')
 
 module.exports = (client) => {
   const channelId = '776603712742359060'
-  firstMessage(client, channelId, "Hatz", [])
+  const getEmoji = (emojiName) =>
+    client.emojis.cache.find((emoji) => emoji.name === emojiName)
+
+  const emojis = {
+    dj: 'DJ',
+    fermecatorul: 'fermecator',
+  }
+
+  const reactions = []
+
+  let emojiText = '**Reactionează pentru a primi un rol:**\n\n'
+  for (const key in emojis) {
+    const emoji = getEmoji(key)
+    reactions.push(emoji)
+
+    const role = emojis[key]
+    emojiText += `${emoji} \`${role}\`\n`
+  }
+
+  firstMessage(client, channelId, emojiText, reactions)
+
+  const handleReaction = (reaction, user, add) => {
+    if (user.id === '658796720213196810') {
+      return
+    }
+
+    const emoji = reaction._emoji.name
+
+    const { guild } = reaction.message
+
+    const roleName = emojis[emoji]
+    if (!roleName) {
+      return
+    }
+
+    const role = guild.roles.cache.find((role) => role.name === roleName)
+    const member = guild.members.cache.find((member) => member.id === user.id)
+
+    if (add) {
+      member.roles.add(role)
+    } else {
+      member.roles.remove(role)
+    }
+  }
+
+  client.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, true)
+    }
+  })
+
+  client.on('messageReactionRemove', (reaction, user) => {
+    if (reaction.message.channel.id === channelId) {
+      handleReaction(reaction, user, false)
+    }
+  })
 }
